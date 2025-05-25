da40f08ada108c1397d75d49c032610e7480db0a
Daniil Iaitskov <dyaitskov@gmail.com>
2025-05-24 17:55:36 -0800

    master remote nix for CI

--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,12 @@
 rec {
-  sources = import ./sources.nix;
+  chooseSources =
+    if builtins.pathExists /home/dan/pro/nixpkgs
+    then ./local.sources.json
+    else
+      if builtins.pathExists ./sources.json
+      then ./sources.json
+      else null;
+  sources = import ./sources.nix { inherit chooseSources; };
   defaultNixpkgs = import sources.nixpkgs;
   pkgSetForSystem = system: args: defaultNixpkgs (args // { inherit system; });
   pkgSet = pkgSetForSystem builtins.currentSystem;
